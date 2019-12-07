20821f26bc14b641faa89903e60c8258ad000539
ayrbox <srdx7@hotmail.com>
2019-12-07 00:40:50 +0000

    Add date range picker with next css loader

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,20 +1,41 @@
 import Head from 'next/head';
 import houses from '../houses.json'
 import Layout from '../../components/Layout';
+import DateRangePicker from '../../components/DateRangePicker'
 
 const House = (props) => (
   <Layout>
-      <Head>
-          <title>{props.house.title}</title>
-      </Head>
-    <img src={props.house.picture} width='100%' alt='House picture' />
-    <p>
-      {props.house.type} - {props.house.town}
-    </p>
-    <p>{props.house.title}</p>
-    <p>
-      {props.house.rating} ({props.house.reviewsCount})
-    </p>
+      <div className="container">
+        <Head>
+            <title>{props.house.title}</title>
+        </Head>
+        <article>
+          <img src={props.house.picture} width='100%' alt='House picture' />
+          <p>
+            {props.house.type} - {props.house.town}
+          </p>
+          <p>{props.house.title}</p>
+          <p>
+            {props.house.rating} ({props.house.reviewsCount})
+          </p>
+        </article>
+        <aside>
+          <h2>Add dates for prices</h2>
+          <DateRangePicker />
+        </aside>
+        <style jsx>{`
+          .container {
+            display: grid;
+            grid-template-columns: 60% 40%;
+            grid-gap: 30px;
+          }
+
+          aside {
+            border: 1px solid #ccc;
+            padding: 20px;
+          }
+        `}</style>
+      </div>
   </Layout>
 )
 
