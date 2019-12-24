3744349218815206bf7e2ac88da3ed82d8e7dcda
ayrbox <srdx7@hotmail.com>
2019-12-24 03:12:39 +0000

    Create users from signup payload

---
+++ database.js
@@ -0,0 +1,4 @@
+export const user = 'next';
+export const password = 'next';
+export const host = 'localhost';
+export const database = 'nextbnb';
--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,7 +1,30 @@
-export default (req, res) => {
+import { User } from '../../../model';
+
+export default async (req, res) => {
     if(req.method !== 'POST') {
         res.state(405).end();
         return;
     }
-    console.log(req.body);
+    console.log(res)
+    
+    const { email, password, passwordConfirmation } = req.body;
+
+    if (password !== passwordConfirmation) {
+        res.statusCode = 400;
+        res.end(JSON.stringify({ status: 'error', message: 'Password do not match' }));
+        return;
+    }
+
+
+    try {
+        const user = await User.create({ email, password });
+        res.end(JSON.stringify({ status: 'success', message: 'User added' }));
+    } catch (error) {
+        res.statusCode = 500;
+        let message = 'Unexpected error';
+        if (error.name === "SequelizeUniqueConstraintError") {
+            message = 'User already exists';
+        }
+        res.end(JSON.stringify({ status: 'error', message }));
+    }
 }
