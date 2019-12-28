e323503529063e7bd9fa3074f64c33711ea40b67
ayrbox <srdx7@hotmail.com>
2019-12-28 03:54:00 +0000

    Booking list for users

--- components/Header.js
+++ components/Header.js
@@ -30,6 +30,11 @@ const Header = () => {
             <>
               <li className="username">{user}</li>
               <li>
+                <Link href="/bookings">
+                  <a>Bookings</a>
+                </Link>
+              </li>
+              <li>
                 <a href='#' onClick={async () => {
                   await axios.post('/api/auth/logout');
                   setUser(null);
